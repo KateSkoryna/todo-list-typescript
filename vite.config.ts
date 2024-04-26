bca570de3d6a8c6da7888664d59861703ed3883b
“Kateryna <k.skoryna@gmail.com>
2024-04-26 13:49:42 +0200

    add comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+//base url
 export default defineConfig({
   plugins: [react()],
   base: "/todo-list-typescript/"
